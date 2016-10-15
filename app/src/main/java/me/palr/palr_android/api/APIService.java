a9cfadf9e98b346e99737b1487088d7f80c97b4c
MaazAli <maaz_ali@outlook.com>
2016-10-15 15:41:21 -0400

    Initialized android project

---
+++ app/src/main/java/me/palr/palr_android/api/APIService.java
@@ -0,0 +1,8 @@
+package me.palr.palr_android.api;
+
+/**
+ * Created by maazali on 2016-10-15.
+ */
+public interface APIService {
+
+}
---
+++ app/src/main/java/me/palr/palr_android/models/User.java
@@ -0,0 +1,12 @@
+package me.palr.palr_android.models;
+
+/**
+ * Created by maazali on 2016-10-15.
+ */
+public class User {
+    String name;
+    String email;
+    String gender;
+    String location;
+    Integer age;
+}
